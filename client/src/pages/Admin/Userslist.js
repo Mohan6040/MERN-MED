7c03b669cb91e25567f95effe38c3d55d897f090
mohankumar <ntrmohan837@gmail.com>
2023-12-22 18:24:33 +0530

    pharmacist added

--- client/src/pages/Admin/Userslist.js
+++ client/src/pages/Admin/Userslist.js
@@ -3,27 +3,23 @@ import { useDispatch } from "react-redux";
 import Layout from "../../components/Layout";
 import { showLoading, hideLoading } from "../../redux/alertsSlice";
 import axios from "axios";
-import { Table, Input, Button, Space } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
+import { Table } from "antd";
 import moment from "moment";
 
 function Userslist() {
   const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
   const dispatch = useDispatch();
-
   const getUsersData = async () => {
     try {
       dispatch(showLoading());
-      const response = await axios.get("/api/admin/get-all-users", {
+      const resposne = await axios.get("/api/admin/get-all-users", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
       dispatch(hideLoading());
-      if (response.data.success) {
-        setUsers(response.data.data);
-        setFilteredUsers(response.data.data);
+      if (resposne.data.success) {
+        setUsers(resposne.data.data);
       }
     } catch (error) {
       dispatch(hideLoading());
@@ -33,16 +29,6 @@ function Userslist() {
   useEffect(() => {
     getUsersData();
   }, []);
-
-  const handleSearch = (value) => {
-    const lowerCaseValue = value.toLowerCase();
-    const filteredData = users.filter(
-      (user) =>
-        user.name.toLowerCase().includes(lowerCaseValue) ||
-        user.email.toLowerCase().includes(lowerCaseValue)
-    );
-    setFilteredUsers(filteredData);
-  };
 
   const columns = [
     {
@@ -56,8 +42,7 @@ function Userslist() {
     {
       title: "Created At",
       dataIndex: "createdAt",
-      render: (text, record) =>
-        moment(record.createdAt).format("DD-MM-YYYY"),
+      render: (record , text) => moment(record.createdAt).format("DD-MM-YYYY"),
     },
     {
       title: "Actions",
@@ -74,14 +59,7 @@ function Userslist() {
     <Layout>
       <h1 className="page-header">Users List</h1>
       <hr />
-      <Space style={{ marginBottom: 16 }}>
-        <Input
-          placeholder="Search by name or email"
-          prefix={<SearchOutlined />}
-          onChange={(e) => handleSearch(e.target.value)}
-        />
-      </Space>
-      <Table columns={columns} dataSource={filteredUsers} />
+      <Table columns={columns} dataSource={users}/>
     </Layout>
   );
 }
